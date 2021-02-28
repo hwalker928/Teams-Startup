6dd4bd072f6b7cf5bdb7065df3da61ed217a5d1a
hwalker928 <mail@harrydev.org>
2021-02-28 13:46:49 +0000

    Removed unnecessary variables
    - Removed unnecessary variables
    - Added comments

--- Teams-Startup-Setup.py
+++ Teams-Startup-Setup.py
@@ -1,15 +1,16 @@
+# Imports
 import datetime
 import subprocess
 import getpass
 import os.path
 from shutil import copyfile
 
-no = datetime.datetime.today().weekday()
-username = getpass.getuser()
-path = r'C:\Users\\' + username + r'\AppData\Local\Microsoft\Teams\current\Teams.exe'
-startup_path = r'C:\Users\\' + username + r'\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup'
-already_exists_in_startup = os.path.isfile(startup_path + "\\teams.py")
-if not already_exists_in_startup:
-    copyfile("Teams-Startup-Setup.py", startup_path + "\\teams.py")
-if no < 5:
-    subprocess.call([path])
+# Variables
+username = getpass.getuser() # Gets the username of the current Windows User, e.g. Administrator, kevin, steve
+startup_path = r'C:\Users\\' + username + r'\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup' # Location of startup folder
+
+# Copying + opening
+if not os.path.isfile(startup_path + "\\teams.py"): # If the file isn't already in the startup folder (usually first-time run)
+    copyfile("Teams-Startup-Setup.py", startup_path + "\\teams.py") # Copy this script to the startup folder
+if datetime.datetime.today().weekday() < 5: # If it is monday - friday
+    subprocess.call([r'C:\Users\\' + username + r'\AppData\Local\Microsoft\Teams\current\Teams.exe']) # Open Teams
